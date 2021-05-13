d3ddbc064462c118ea703a703567734b7742bbf3
Seth Gregory <sethgregory@seths-mbp.myfiosgateway.com>
2021-05-12 22:45:39 -0400

    changed app

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -33,31 +33,22 @@ from .routes import main
 from.users.routes import users
 from .artist.routes import artists
 
+SECRET_KEY = b'\x020;yr\x21\x81\xbe"\x9d\xc1\x14\x51\xadf\xec'
 
-def page_not_found(e):
-    return render_template("404.html"), 404
+app = Flask(__name__)
 
+app.config["MONGODB_HOST"] = os.getenv("MONGODB_HOST")
 
-def create_app(test_config=None):
-    app = Flask(__name__)
+db.init_app(app)
+login_manager.init_app(app)
+bcrypt.init_app(app)
 
-    app.config["MONGODB_HOST"] = os.getenv("MONGODB_HOST")
-    app.config.from_pyfile("config.py", silent=False)
-    if test_config is not None:
-        app.config.update(test_config)
+# REGISTER BLUEPRINTS HERE
+app.register_blueprint(main)
+app.register_blueprint(users)
+app.register_blueprint(artists)
 
-    db.init_app(app)
-    login_manager.init_app(app)
-    bcrypt.init_app(app)
+map_client.save_html(map_client.basic_map, "basic_map.html")
+map_client.save_html(map_client.heat_map, "heat_map.html")
 
-    # REGISTER BLUEPRINTS HERE
-    app.register_blueprint(main)
-    app.register_blueprint(users)
-    app.register_blueprint(artists)
-
-    map_client.save_html(map_client.basic_map, "basic_map.html")
-    map_client.save_html(map_client.heat_map, "heat_map.html")
-
-    login_manager.login_view = "users.login"
-
-    return app
+login_manager.login_view = "users.login"
--- flask_app/config.py
+++
@@ -1,3 +0,0 @@
-
-# Configuration values
-SECRET_KEY = b'\x020;yr\x21\x81\xbe"\x9d\xc1\x14\x51\xadf\xec'
